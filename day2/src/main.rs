62b6b25be2450e1c50a53b700822fef4de02033f
ErnestoMLopez <ernesto.lopez@ing.unlp.edu.ar>
2024-12-06 15:18:00 -0300

    Solución de la parte 1 del día 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     let file = fs::File::open(input_file).expect("Error opening file");
     let reader = io::BufReader::new(file);
 
+    let mut safe_report = 0;
+
     for line in reader.lines() {
         let line = line.expect("Error reading line");
 
@@ -18,6 +20,16 @@ fn main() {
 
         let diffnumbers: Vec<i32> = numbers.windows(2).map(|x| x[1] - x[0]).collect();
 
-        println!("{:?}", diffnumbers);
+        let positive_steps = diffnumbers.iter().filter(|&&x| x > 0).count();
+        let negative_steps = diffnumbers.iter().filter(|&&x| x < 0).count();
+
+        let monotonic = positive_steps == diffnumbers.len() || negative_steps == diffnumbers.len();
+        let bounded = diffnumbers.iter().map(|x| x.abs()).max().unwrap() <= 3;
+
+        if monotonic && bounded {
+            safe_report += 1;
+        }
     }
+
+    println!("Safe reports: {}", safe_report);
 }
