ca0416719be5ae6f571f916127ea48c38a7656fe
ErnestoMLopez <ernesto.lopez@ing.unlp.edu.ar>
2024-12-18 16:19:27 -0300

    Parseo de la grilla a posición de obstaculos

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,27 @@
 pub mod mat;
 
 use mat::Matrix;
+use std::collections::HashMap;
 use std::fs;
 use std::io;
 use std::io::Read;
+
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+struct Obstacles {
+    per_row: HashMap<usize, Vec<usize>>,
+    per_col: HashMap<usize, Vec<usize>>,
+}
 
 fn main() {
     println!("Advent of Code 2024 - Day 4");
@@ -19,5 +37,37 @@ fn main() {
 
     let matrix = Matrix::from_string(&buffer);
 
+    let (guard, obstacles) = parse_input(&matrix);
+
     println!("Fin")
+}
+
+fn parse_input(grid: &Matrix<char>) -> (Position, Obstacles) {
+    let mut guard = Position { x: 0, y: 0 };
+    let mut obstacles = Obstacles {
+        per_row: HashMap::new(),
+        per_col: HashMap::new(),
+    };
+
+    for row in 0..grid.rows() {
+        for col in 0..grid.cols() {
+            match grid.get(row, col) {
+                '^' => guard = Position { x: row, y: col },
+                '#' => {
+                    if !obstacles.per_row.contains_key(&row) {
+                        obstacles.per_row.insert(row, Vec::new());
+                    }
+                    obstacles.per_row.get_mut(&row).unwrap().push(col);
+
+                    if !obstacles.per_col.contains_key(&col) {
+                        obstacles.per_col.insert(col, Vec::new());
+                    }
+                    obstacles.per_col.get_mut(&col).unwrap().push(row);
+                }
+                _ => (),
+            }
+        }
+    }
+
+    (guard, obstacles)
 }
