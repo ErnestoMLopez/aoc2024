099e1f338b0dd3c06245be4b5923a5a07b6a6dd4
ErnestoMLopez <ernestomlopez@outlook.com>
2024-12-18 23:42:15 -0300

    Genero correctamente la lista de obstáculos

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,16 +1,19 @@
 pub mod mat;
 
 use mat::Matrix;
+use std::collections::BinaryHeap;
 use std::collections::HashMap;
 use std::fs;
 use std::io;
 use std::io::Read;
 
+#[derive(Debug)]
 struct Position {
     x: usize,
     y: usize,
 }
 
+#[derive(Debug)]
 enum Direction {
     Up,
     Down,
@@ -18,6 +21,13 @@ enum Direction {
     Right,
 }
 
+#[derive(Debug)]
+struct Guard {
+    position: Position,
+    direction: Direction,
+}
+
+#[derive(Debug)]
 struct Obstacles {
     per_row: HashMap<usize, Vec<usize>>,
     per_col: HashMap<usize, Vec<usize>>,
@@ -37,31 +47,45 @@ fn main() {
 
     let matrix = Matrix::from_string(&buffer);
 
-    let (guard, obstacles) = parse_input(&matrix);
+    let (guard, obstacles) = generate_map(&matrix);
 
-    println!("Fin")
+    println!("Guard: {:?}", guard);
+    println!("Obstacles: Row 0: {:?}", obstacles.per_row.get(&0).unwrap());
+    println!("Obstacles: Col 0: {:?}", obstacles.per_col.get(&0).unwrap());
 }
 
-fn parse_input(grid: &Matrix<char>) -> (Position, Obstacles) {
-    let mut guard = Position { x: 0, y: 0 };
+fn generate_map(grid: &Matrix<char>) -> (Guard, Obstacles) {
+    let mut guard = Guard {
+        position: Position { x: 0, y: 0 },
+        direction: Direction::Up,
+    };
     let mut obstacles = Obstacles {
         per_row: HashMap::new(),
         per_col: HashMap::new(),
     };
 
+    // Creo las entradas para cada fila y columna. Si no hay obstaculos en una fila/columna el
+    // vector estará vacío, pero puedo buscar igual
+    for row in 0..grid.rows() {
+        obstacles.per_row.insert(row, Vec::new());
+    }
+
+    for col in 0..grid.cols() {
+        obstacles.per_col.insert(col, Vec::new());
+    }
+
+    // Recorro la grilla buscando la posición inicial del guardia y los obstáculos: Esta forma de
+    // recorrerla me asegura que los vectores quedan automáticamente ordenados de menor a mayor
     for row in 0..grid.rows() {
         for col in 0..grid.cols() {
             match grid.get(row, col) {
-                '^' => guard = Position { x: row, y: col },
+                '^' => {
+                    guard.position.x = row;
+                    guard.position.y = col;
+                    guard.direction = Direction::Up
+                }
                 '#' => {
-                    if !obstacles.per_row.contains_key(&row) {
-                        obstacles.per_row.insert(row, Vec::new());
-                    }
                     obstacles.per_row.get_mut(&row).unwrap().push(col);
-
-                    if !obstacles.per_col.contains_key(&col) {
-                        obstacles.per_col.insert(col, Vec::new());
-                    }
                     obstacles.per_col.get_mut(&col).unwrap().push(row);
                 }
                 _ => (),
