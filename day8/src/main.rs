0d657b554c03eab70a2ba298ed27c17fa7a65183
ErnestoMLopez <ernestomlopez@outlook.com>
2024-12-30 00:25:28 -0300

    Solución día 8 parte 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -39,8 +39,10 @@ fn main() {
     let antennas = get_antennas(&matrix);
 
     let antinodes = create_antinodes(&antennas);
+    let harmonic_antinodes = create_harmonic_antinodes(&antennas);
 
     println!("Part 1: {:?} antinodes", antinodes.len());
+    println!("Part 2: {:?} Harmonic antinodes", harmonic_antinodes.len());
 
     // Imprimo el tiempo de ejecución
     let elapsed = now.elapsed();
@@ -92,6 +94,29 @@ fn create_antinodes(antenna_map: &AntennaMap) -> HashMap<Position, Vec<char>> {
     antinodes
 }
 
+fn create_harmonic_antinodes(antenna_map: &AntennaMap) -> HashMap<Position, Vec<char>> {
+    let mut antinodes = HashMap::new();
+
+    antenna_map
+        .antennas
+        .iter()
+        .for_each(|(antenna, positions)| {
+            positions.iter().enumerate().for_each(|(i, p1)| {
+                positions.iter().skip(i + 1).for_each(|p2| {
+                    let a = calculate_harmonic_antinodes(p1, p2, antenna_map);
+                    a.iter().for_each(|antinode| {
+                        antinodes
+                            .entry(*antinode)
+                            .or_insert(Vec::new())
+                            .push(*antenna);
+                    });
+                });
+            });
+        });
+
+    antinodes
+}
+
 fn calculate_antinodes(
     pos1: &Position,
     pos2: &Position,
@@ -126,4 +151,46 @@ fn calculate_antinodes(
 
 fn is_valid(x: isize, y: isize, antenna_map: &AntennaMap) -> bool {
     x < antenna_map.rows as isize && y < antenna_map.cols as isize && x >= 0 && y >= 0
+}
+
+fn calculate_harmonic_antinodes(
+    pos1: &Position,
+    pos2: &Position,
+    antenna_map: &AntennaMap,
+) -> Vec<Position> {
+    let mut antinodes = Vec::new();
+
+    let dx = pos2.x as isize - pos1.x as isize;
+    let dy = pos2.y as isize - pos1.y as isize;
+
+    let mut n = 0;
+
+    loop {
+        let a1_x = pos2.x as isize + dx * n;
+        let a1_y = pos2.y as isize + dy * n;
+        let a2_x = pos1.x as isize - dx * n;
+        let a2_y = pos1.y as isize - dy * n;
+
+        if !is_valid(a1_x, a1_y, antenna_map) && !is_valid(a2_x, a2_y, antenna_map) {
+            break;
+        }
+
+        if is_valid(a1_x, a1_y, antenna_map) {
+            antinodes.push(Position {
+                x: a1_x as usize,
+                y: a1_y as usize,
+            });
+        }
+
+        if is_valid(a2_x, a2_y, antenna_map) {
+            antinodes.push(Position {
+                x: a2_x as usize,
+                y: a2_y as usize,
+            });
+        }
+
+        n += 1;
+    }
+
+    antinodes
 }
